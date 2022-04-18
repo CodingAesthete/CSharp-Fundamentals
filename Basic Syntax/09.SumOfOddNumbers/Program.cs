ea3b924fd9fd5604fc8ad32cc40d41e8bd8fa08c
PostPunkAesthete <harry_ollI@abv.bg>
2022-04-18 15:03:18 +0300

    Initial commit

---
+++ Basic Syntax/09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09.SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNums = int.Parse(Console.ReadLine());
+            int currNum = 1;
+            int sum = 0;
+
+            for (int i = 0; i < countNums; i++)
+            {
+                Console.WriteLine(currNum);
+                sum += currNum;
+                currNum += 2;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ Basic Syntax/12.EvenNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _12.EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            while (Math.Abs(num) % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                num = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"The number is: {Math.Abs(num)}");
+            return;
+        }
+    }
+}
